d9412dc851b59652a91e4cae25ed4e4702a43a01
Savannah C <sgcorrell@gmail.com>
2019-10-23 16:22:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function mapToNegativize(sourceArray) {
   const negatives = []
   for (const number of sourceArray) {
-    newNegative = number * -1
+    const newNegative = number * -1
     negatives.push(newNegative)
   }
   return negatives
